335a835cb1f7371757fc4e2cfe653e4ae9a6855f
DementiaGaming <sukhrathansidhu@outlook.com>
2025-04-01 21:21:32 +0100

    more sounds

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ keys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
           "a", "s", "d", "f", "g", "h", "j", "k", "l",
           "z", "x", "c", "v", "b", "n", "m"]
 
-sounds = ["jonkler.mp3"]
+sounds = ["jonkler.mp3", "WindowsXPShutdown.mp3", "error.mp3", "metalPipe.mp3"]
 
 while running:
     for i in range(len(keys)):
         tempKey = keys[i]
         if keyboard.is_pressed(tempKey):
             print("t")
-            if random.randint(1, 10000) == 1:
+            if random.randint(1, 1000) == 1:
                 tempSound = random.choice(sounds)
-                playsound.playsound(f"sounds/{tempSound}.mp3", False)
-    
-    
+                playsound.playsound(f"sounds/{tempSound}", False)
