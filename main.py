fc9c0418ddd297ac17bf3d65b21bbdce32b4dea9
DementiaGaming <sukhrathansidhu@outlook.com>
2025-04-03 19:10:49 +0100

    metal pole

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ keys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
           "a", "s", "d", "f", "g", "h", "j", "k", "l",
           "z", "x", "c", "v", "b", "n", "m"]
 
-sounds = ["jonkler.mp3", "WindowsXPShutdown.mp3", "error.mp3", "metalPipe.mp3"]
-
 while running:
     for i in range(len(keys)):
         tempKey = keys[i]
         if keyboard.is_pressed(tempKey):
             print("t")
-            if random.randint(1, 1000) == 1:
-                tempSound = random.choice(sounds)
-                playsound.playsound(f"sounds/{tempSound}", False)
+            playsound.playsound(f"sounds/metalPipe.mp3", False)
